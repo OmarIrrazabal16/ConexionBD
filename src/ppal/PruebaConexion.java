7e27f4f5422d60f3042c44e43d8955e8c5397fa3
Desarrollo <Desarrollo@DESARROLLO07.local>
2024-05-30 11:18:05 +0200

    Clase  Conexion completa

---
+++ src/ppal/PruebaConexion.java
@@ -0,0 +1,22 @@
+package ppal;
+
+import java.sql.Connection;
+
+import conexion.ConexionBD;
+
+public class PruebaConexion {
+
+	public static void main(String[] args) {
+		
+		ConexionBD conexion = new ConexionBD();
+		System.out.println("Conectando a la base de datos...");
+		Connection con = conexion.getConexion();	
+		
+		//Algún procesamiento con la base de datos..
+		
+		//Lberamos la conexión
+		conexion.deconectar();
+
+	}
+
+}
